e8b04141f5e7fa880c93161bd826e5cb9972ee36
Carl Andersson <carl@carlandersson.net>
2024-12-23 18:51:43 +0100

    Prepare last days

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ async function run_profile(day: number, part: number) {
         console.log(result)
     }
 }
-const latest_day = 22
+const latest_day = 25
 
 import Benchmark, { Event } from 'benchmark'
 import { prettylog } from './helpers/prettybench'
